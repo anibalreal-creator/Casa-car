acf570c24a146c1c11bdbaa8469b7045b604404e
anibalreal-creator <anibalreal@hotmail.com>
2026-03-07 21:48:13 -0300

    Actualizar supabaseAdmin.js

--- web/lib/supabaseAdmin.js
+++ web/lib/supabaseAdmin.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js"
 
-throw new Error("PRUEBA_SUPABASEADMIN")
+const supabase = createClient(
+  "https://cdmlyrjccdxakvbmbbpp.supabase.co",
+  "sb_publishable_j6ibbVlh7A_S4ww4P3wUEg_YxgzDO1u"
+)
 
-const supabaseUrl = "https://cdmlyrjccdxakvbmbbpp.supabase.co"
-const supabaseKey = "sb_publishable_j6ibbVlh7A_S4ww4P3wUEg_YxgzDO1u"
-
-export const supabaseAdmin = createClient(supabaseUrl, supabaseKey)
+export { supabase }
