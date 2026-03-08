29849f611698eec347792819e2aed181eb2a85e8
anibalreal-creator <anibalreal@hotmail.com>
2026-03-07 21:25:55 -0300

    Actualizar supabaseAdmin.js

--- web/lib/supabaseAdmin.js
+++ web/lib/supabaseAdmin.js
@@ -1,17 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl =
-  process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL
-
-const supabaseKey =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY
-
-if (!supabaseUrl) {
-  throw new Error("SUPABASE_URL no está definido")
-}
-
-if (!supabaseKey) {
-  throw new Error("SUPABASE_KEY no está definido")
-}
+const supabaseUrl = "https://cdmlyrjccdxakvbmbbpp.supabase.co"
+const supabaseKey = "sb_publishable_j6ibbVlh7A_S4ww4P3wUEg_YxgzDO1u"
 
 export const supabaseAdmin = createClient(supabaseUrl, supabaseKey)
