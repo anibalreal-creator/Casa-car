af21654a267aa6ef320cf709b260ddec956b7eab
anibalreal-creator <anibalreal@hotmail.com>
2026-03-07 21:49:50 -0300

    Actualizar listings.js

--- web/pages/api/listings.js
+++ web/pages/api/listings.js
@@ -1,5 +1,4 @@
-import { supabaseAdmin } from "../../lib/supabaseAdmin"
-
+import { supabase } from "../../lib/supabaseAdmin"
 export default async function handler(req, res) {
   try {
     const { data, error } = await supabaseAdmin
