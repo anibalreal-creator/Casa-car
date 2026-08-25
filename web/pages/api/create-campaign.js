b2c61f818a1847861102d4d5e0a1be715889b900
Casa-Car <anibalreal@hotmail.com>
2026-08-24 23:46:54 -0300

    Restore original advertising prices

--- web/pages/api/create-campaign.js
+++ web/pages/api/create-campaign.js
@@ -6,9 +6,9 @@ import { checkRateLimit } from '../../lib/server/rateLimit';
 import { getSupabaseServer } from '../../lib/supabaseServer';
 
 const PLAN_PRICES = {
-  basico: 500,
-  destacado: 1000,
-  premium: 1500,
+  basico: 25000,
+  destacado: 65000,
+  premium: 145000,
 };
 
 const PLAN_DAYS = {
